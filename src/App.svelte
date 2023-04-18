e0ecc762b134f318957f2199e3d689bfc7e2b6ec
giacomobg <pepperroni@hotmail.co.uk>
2023-04-18 02:26:16 +0100

    Make sure bar chart is removed

--- src/App.svelte
+++ src/App.svelte
@@ -46,14 +46,14 @@
 
 <div class="embed-container">
   <h1>Damage to educational facilities in Ukraine</h1>
-  <h2>From February 2022 to February 2023</h2>
+  <h2>February 2022 to February 2023</h2>
   <!-- <h2>{time.toLocaleString("en-GB", { year: "numeric", month: "long"})}</h2> -->
-  <TimeControls bind:timeIndex={timeIndex} {timePeriods} animated={true}></TimeControls>
+  <TimeControls bind:timeIndex={timeIndex} {timePeriods} animated={false}></TimeControls>
   <div class="vis-container">
       <Map {time} />
-      <Cartesian>
+      <!-- <Cartesian>
         <Column/> 
-      </Cartesian>
+      </Cartesian> -->
   </div>
   <!-- <div class="source-text">Note:</div> -->
   <div class="source-text">Source: Centre for Information Resilience</div>
